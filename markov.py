8f6ab1b4bca92759876dfcb86355a7d2116ec1be
ryder-mcdowell <Ryder@10-116-20-37.wireless.oregonstate.edu>
2017-04-18 19:46:57 -0700

    Added files to repository

---
+++ markov.py
@@ -0,0 +1,31 @@
+import random
+
+class Markov:
+
+    def __init__(self):
+        self.model = {}
+        self.state = (None, None)
+
+    # Add `word` to the model.
+    def add(self, word):
+        if self.state in self.model:
+            self.model[self.state].append(word)
+        else:
+            self.model[self.state] = [word]
+        self.transition(word)
+
+    # Reset the current state of the Markov chain.
+    def reset(self):
+        self.state = (None, None)
+
+    # Return the word that likely follows the current state.
+    def next_word(self):
+        words = self.model[self.state]
+        word = random.choice(words)
+        self.transition(word)
+        return word
+
+    # Transition into the next state.
+    # Example: (a, b) --> (b, next)
+    def transition(self, next):
+        self.state = (self.state[1], next)
